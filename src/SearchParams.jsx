1258e18f942dc762bfa6185f7aa8b34ee2f5e6d9
Jeff Chang <jeffjmc4@gmail.com>
2023-08-24 15:12:55 -0700

    new project added

---
+++ src/SearchParams.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { useState } from "react";
+const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
+
+const SearchParams = () => {
+  const [location, setLocation] = useState("");
+  const [animal, setAnimal] = useState("");
+
+  return (
+    <div className="search-params">
+      <form>
+        <label htmlFor="location">
+          Location
+          <input
+            onChange={(e) => setLocation(e.target.value)}
+            id="location"
+            value={location}
+            placeholder="Location"
+          />
+          <button>Submit</button>
+        </label>
+        <label htmlFor="animal">
+          Animal
+          <select
+            id="animal"
+            value={animal}
+            onChange={(e) => {
+              setAnimal(e.target.value);
+            }}
+          >
+            <option>--------</option>
+            {ANIMALS.map((animal) => (
+              <option key={animal}>{animal}</option>
+            ))}
+          </select>
+        </label>
+      </form>
+    </div>
+  );
+};
+
+export default SearchParams;
